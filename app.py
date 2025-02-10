9945d153e0cb8beb75cd51867a2070f175dbabbe
fatiha-51 <boudehanefatiha51@gmail.com>
2025-02-10 17:17:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-# app.py
-
 from flask import Flask, request, jsonify
 import pytesseract
 from PIL import Image
 import io
 
+# Initialisation de l'application Flask
 app = Flask(__name__)
 
 # Chemin vers l'exécutable Tesseract (automatiquement configuré dans le conteneur)
@@ -13,10 +12,13 @@ pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract"
 @app.route('/ocr', methods=['POST'])
 def ocr():
     try:
+        # Vérifier si un fichier a été uploadé
         if 'file' not in request.files:
             return jsonify({"error": "Aucun fichier uploadé"}), 400
         
         file = request.files['file']
+        
+        # Vérifier si le nom du fichier est valide
         if file.filename == '':
             return jsonify({"error": "Nom de fichier invalide"}), 400
         
@@ -26,10 +28,13 @@ def ocr():
         # Extraire le texte avec Tesseract
         text = pytesseract.image_to_string(img)
         
+        # Renvoyer le texte extrait sous forme de JSON
         return jsonify({"text": text})
     
     except Exception as e:
+        # Gérer les erreurs et renvoyer un message d'erreur
         return jsonify({"error": str(e)}), 500
 
+# Démarrer l'application Flask en mode debug (seulement pour le développement local)
 if __name__ == '__main__':
     app.run(debug=True)
